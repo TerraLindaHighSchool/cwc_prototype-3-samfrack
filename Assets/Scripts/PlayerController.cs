a4c4cf4a8ce17cc9b27adbd4d0ad8416a132ae1c
samfrack <89489598+samfrack@users.noreply.github.com>
2022-05-03 17:41:03 -0500

    Lesson 3.4.7 Complete

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,11 @@ public class PlayerController : MonoBehaviour
     public bool isOnGround = true;
     public bool gameOver;
     private Animator playerAnim;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    private AudioSource playerAudio;
     
 
     // Start is called before the first frame update
@@ -18,34 +23,44 @@ public class PlayerController : MonoBehaviour
         playerRb = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
+        playerAudio = GetComponent<AudioSource>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Makes player jump
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             Debug.Log("PlayerController:24" + jumpForce);
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
         }
         
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        // Checks to see if player is on ground before allowing player to jump
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            dirtParticle.Play();
         }
+        // Sets game over and sets game over animation if player hits obstacle
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
             gameOver = true;
             Debug.Log("Game Over!");
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound);
         }
     }
 }
